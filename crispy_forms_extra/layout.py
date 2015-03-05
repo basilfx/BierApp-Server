60a92a19cfcd0cd4aeeee13232d1c0f29782ea07
Bas Stottelaar <basstottelaar@gmail.com>
2015-03-05 12:25:12 +0100

    Unfold short names.

--- crispy_forms_extra/layout.py
+++ crispy_forms_extra/layout.py
@@ -5,30 +5,33 @@ from django.template import Context
 from crispy_forms.layout import Field, TEMPLATE_PACK
 
 class BaseTable(object):
-    template = "%s/table_elems.html" % TEMPLATE_PACK
- 
+    template = "%s/table_elements.html" % TEMPLATE_PACK
+
     def __init__(self, *fields, **kwargs):
         self.fields = fields
- 
-        if hasattr(self, 'css_class') and 'css_class' in kwargs:
-            self.css_class += ' %s' % kwargs.get('css_class')
-        if not hasattr(self, 'css_class'):
-            self.css_class = kwargs.get('css_class', None)
- 
-        self.css_id = kwargs.get('css_id', '')
-        self.template = kwargs.get('template', self.template)
- 
+
+        if hasattr(self, "css_class") and "css_class" in kwargs:
+            self.css_class += " %s" % kwargs.get("css_class")
+        if not hasattr(self, "css_class"):
+            self.css_class = kwargs.get("css_class", None)
+
+        self.css_id = kwargs.get("css_id", "")
+        self.template = kwargs.get("template", self.template)
+
     def render(self, form, form_style, context, template_pack):
-        fields = ''
+        fields = ""
+
         for field in self.fields:
             fields += render_field(field, form, form_style, context)
- 
-        return render_to_string(self.template, Context({'tag': self.element,
-                                                        'elem': self,
-                                                        'fields': fields}))
+
+        return render_to_string(
+            self.template, Context({
+                "tag": self.element,
+                "element": self,
+                "fields": fields}))
 
 class Td(BaseTable):
-    element = 'td'
+    element = "td"
 
 class Tr(BaseTable):
-    element = 'tr'
+    element = "tr"
