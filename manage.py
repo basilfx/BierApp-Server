f01245f118f70b7700db745dc191acf4cd60f82a
Bas Stottelaar <basstottelaar@gmail.com>
2017-06-24 16:33:40 +0200

    Move dependencies into subdirectory. Update requirements.

--- manage.py
+++ manage.py
@@ -4,14 +4,16 @@ import sys
 
 
 if __name__ == "__main__":
-    # Append path with this project
+    # Append path with this project and libraries.
     sys.path.insert(
         0, os.path.join(os.path.abspath(os.path.dirname(__file__)), "../"))
+    sys.path.insert(
+        1, os.path.join(os.path.abspath(os.path.dirname(__file__)), "lib/"))
 
-    # Set settings file
+    # Set settings file.
     os.environ.setdefault(
         "DJANGO_SETTINGS_MODULE", "bierapp.settings.local")
 
-    # Load Django environment
+    # Load Django environment.
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
